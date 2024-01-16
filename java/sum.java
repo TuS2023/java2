aa6555e9fe3e6b650fa8eb11c38a3c1593be6a6a
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2024-01-16 06:01:46 +0000

    お金の計算プログラム

---
+++ java/sum.java
@@ -0,0 +1,26 @@
+public class Code003{
+  public static void main(String[] args) {
+    int x_10000 = 0;
+    int x_5000 = 0;
+    int x_1000 = 0;
+    int x_500 = 0;
+    int x_100 = 0;
+    int x_50 = 0;
+    int x_10 = 0;
+    int x_5 = 0;
+    int x_1 = 0;
+    
+    int ans = x_10000*10000 +
+      x_5000*5000 +
+      x_1000*1000 +
+      x_500*500 +
+      x_100*100 +
+      x_50*50 +
+      x_10*10 +
+      x_5*5 +
+      x_1*1;
+  
+  System.out.println(ans);
+  
+ }
+} 
