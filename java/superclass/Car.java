547bc726061044154773fd53e9757048f3a6687c
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2024-01-25 11:23:39 +0000

    super

--- java/superclass/Car.java
+++ java/superclass/Car.java
@@ -7,9 +7,7 @@ class Car extends Vehicle {
   }
   
   public void printData() {
-    System.out.println("名前：" + this.getName());
-    System.out.println("色：" + this.getColor());
-    System.out.println("走行距離：" + this.getDistance() + "km");
+    super.printData();
     System.out.println("ガソリン量：" + this.fuel + "L");
   }
   
