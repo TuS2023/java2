743fa70666b475c580f3efec364d608d217e901e
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2024-01-25 11:08:55 +0000

    メソッドのオーバーライド

--- java/superclass/Main.java
+++ java/superclass/Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
     
+    Scanner scanner = new Scanner(System.in);
     Car car = new Car();
     car.setName("フェラーリ");
     car.setColor("赤");
@@ -11,7 +12,8 @@ class Main {
     
     System.out.println("【車の情報】");
     car.printData();
-    System.out.println("ガソリン量："+ car.getFuel() +"L");
+    
+    System.out.println("-----------------");
     System.out.print("給油する量を入力してください：");
     int litre = scanner.nextInt();
     car.charge(litre);
